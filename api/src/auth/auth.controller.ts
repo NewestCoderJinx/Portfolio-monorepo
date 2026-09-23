e2084f1c5cf76f834e5038b0a5f49af457431a90
NewestCoderJinx <tumwesigyelonnah@gmail.com>
2026-09-23 12:29:15 +0300

    updated tsconfig without type module,attached swagger decorators in auth controller

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,18 +1,22 @@
 import { Controller, Post, Body } from '@nestjs/common';
+import { ApiTags, ApiOperation } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
-import { AuthCredentialsDto } from './dto/auth.credentials.dto';
+import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
+@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
-  register(@Body() dto: AuthCredentialsDto) {
-    return this.authService.register(dto.email, dto.password);
+  @ApiOperation({ summary: 'Register a new admin user' })
+  register(@Body() credentialsDto: AuthCredentialsDto) {
+    return this.authService.register(credentialsDto.email, credentialsDto.password);
   }
 
   @Post('login')
-  login(@Body() dto: AuthCredentialsDto) {
-    return this.authService.login(dto.email, dto.password);
+  @ApiOperation({ summary: 'User login to receive JWT token' })
+  login(@Body() credentialsDto: AuthCredentialsDto) {
+    return this.authService.login(credentialsDto.email, credentialsDto.password);
   }
 }
