1aa1e024e00af25ff333cf13567e2e7ec3f72956
NewestCoderJinx <tumwesigyelonnah@gmail.com>
2026-09-23 11:29:25 +0300

    editing errors on the files.

--- api/src/auth/auth.service.ts
+++ api/src/auth/auth.service.ts
@@ -1,6 +1,6 @@
 import { Injectable, UnauthorizedException, ConflictException } from '@nestjs/common';
 import { JwtService } from '@nestjs/jwt';
-import { UsersService } from './users/users.service';
+import { UsersService } from './user.service';
 import * as bcrypt from 'bcrypt';
 
 @Injectable()
