77a5c3f3063b57cdb7e0c985a8a5243a8594e633
NewestCoderJinx <tumwesigyelonnah@gmail.com>
2026-09-23 13:02:28 +0300

    Fixed typescript importing empty modules.

--- api/src/auth/auth.controller.ts
+++ api/src/auth/auth.controller.ts
@@ -1,22 +1,18 @@
 import { Controller, Post, Body } from '@nestjs/common';
-import { ApiTags, ApiOperation } from '@nestjs/swagger';
 import { AuthService } from './auth.service';
 import { AuthCredentialsDto } from './dto/auth-credentials.dto';
 
-@ApiTags('auth')
 @Controller('auth')
 export class AuthController {
   constructor(private readonly authService: AuthService) {}
 
   @Post('register')
-  @ApiOperation({ summary: 'Register a new admin user' })
-  register(@Body() credentialsDto: AuthCredentialsDto) {
-    return this.authService.register(credentialsDto.email, credentialsDto.password);
+  register(@Body() dto: AuthCredentialsDto) {
+    return this.authService.register(dto.email, dto.password);
   }
 
   @Post('login')
-  @ApiOperation({ summary: 'User login to receive JWT token' })
-  login(@Body() credentialsDto: AuthCredentialsDto) {
-    return this.authService.login(credentialsDto.email, credentialsDto.password);
+  login(@Body() dto: AuthCredentialsDto) {
+    return this.authService.login(dto.email, dto.password);
   }
 }
